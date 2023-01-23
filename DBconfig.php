2f9795f880a7ed859250158d5038075cb49e8c53
abhinand9772 <100460106+abhinand9772@users.noreply.github.com>
2023-01-23 13:03:23 +0530

    updt

--- DBconfig.php
+++
@@ -1,5 +0,0 @@
-$conn = mysqli_connect('localhost', 'root', '', 'gconcept');
-//Check for connection error
-if($conn->connect_error){
-  die("Error in DB connection: ".$conn->connect_errno." : ".$conn->connect_error);    
-}
